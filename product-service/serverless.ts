e6dae40e0b4df207c9e4f969ad1e68e17452eb16
YRAL375 <vvoitkus@mail.ru>
2021-09-01 17:16:20 +0300

    add handlers for product-service

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -1,6 +1,7 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
+import getProductsList from '@functions/getProductsList';
+import getProductsById from '@functions/getProductsById';
 
 const serverlessConfiguration: AWS = {
   service: 'product-service',
@@ -27,7 +28,7 @@ const serverlessConfiguration: AWS = {
     lambdaHashingVersion: '20201221',
   },
   // import the function via paths
-  functions: { hello },
+  functions: { getProductsList, getProductsById }
 };
 
 module.exports = serverlessConfiguration;
