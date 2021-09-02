e3324bededd487249810096f2c59ba3379959f52
YRAL375 <vvoitkus@mail.ru>
2021-09-02 20:04:30 +0300

    update mock data

--- product-service/src/mock-data/books.ts
+++ product-service/src/mock-data/books.ts
@@ -6,9 +6,11 @@ export const books = [
         "kind": "books#volume",
         "id": "_ojXNuzgHRcC",
         "etag": "OTD2tB19qn4",
+        "title": "Practical MongoDB",
+        "price": "$32.04",
+        "image": "https://itbook.store/img/books/9781484206485.png",
         "selfLink": "https://www.googleapis.com/books/v1/volumes/_ojXNuzgHRcC",
         "volumeInfo": {
-            "title": "Flowers",
             "authors": [
                 "Vijaya Khisty Bodach"
             ]
@@ -18,9 +20,11 @@ export const books = [
         "kind": "books#volume",
         "id": "RJxWIQOvoZUC",
         "etag": "NsxMT6kCCVs",
+        "price": "$47.11",
+        "image": "https://itbook.store/img/books/9781484211830.png",
         "selfLink": "https://www.googleapis.com/books/v1/volumes/RJxWIQOvoZUC",
+        "title": "The Definitive Guide to MongoDB, 3rd Edition",
         "volumeInfo": {
-            "title": "Flowers",
             "authors": [
                 "Gail Saunders-Smith"
             ]
@@ -30,9 +34,11 @@ export const books = [
         "kind": "books#volume",
         "id": "zaRoX10_UsMC",
         "etag": "pm1sLMgKfMA",
+        "price": "$32.10",
+        "image": "https://itbook.store/img/books/9781617291609.png",
         "selfLink": "https://www.googleapis.com/books/v1/volumes/zaRoX10_UsMC",
+        "title": "MongoDB in Action, 2nd Edition",
         "volumeInfo": {
-            "title": "Flowers",
             "authors": [
                 "Paul McEvoy"
             ]
